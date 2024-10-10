ae13df975f357a405b5ce7a394ae0544b71d4a24
Michiel Rogissart <michiel@datavillage.me>
2024-10-10 17:33:10 +0200

    add basic import from package

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,5 +1,7 @@
+import { SOLID } from "../../packages/pod-client/solid";
+
 function Login() {
-  return <p>Hello world from login page</p>;
+  return <p>Hello world from login page {SOLID}</p>;
 }
 
 export default Login;
