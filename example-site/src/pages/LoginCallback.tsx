093ba4a0bd07035cfdb79259d8bfcf0ddcba23d7
Michiel Rogissart <michiel@datavillage.me>
2024-10-14 15:19:48 +0200

    remove redundant variable

--- example-site/src/pages/LoginCallback.tsx
+++ example-site/src/pages/LoginCallback.tsx
@@ -1,5 +1,5 @@
 import { finishLogin } from "@datavillage-me/pod-client";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 
 const callbackFunction = async () => {
   const pod = await finishLogin();
@@ -12,14 +12,9 @@ const callbackFunction = async () => {
 };
 
 export default function LoginCallback() {
-  const [isFirst, setIsFirst] = useState<boolean>(true);
-
   useEffect(() => {
-    if (isFirst) {
-      setIsFirst(false);
-      callbackFunction();
-    }
-  }, [isFirst]);
+    callbackFunction();
+  }, []);
 
   return <>Finishing login process...</>;
 }
