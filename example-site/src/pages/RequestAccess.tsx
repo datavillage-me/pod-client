2b07a2e38a8159294eb9d8a122a9796114de1de3
Michiel Rogissart <michiel@datavillage.me>
2024-10-23 15:53:26 +0200

    make webid and file user input

--- example-site/src/pages/RequestAccess.tsx
+++ example-site/src/pages/RequestAccess.tsx
@@ -1,29 +1,52 @@
 import { UmaPod } from "@datavillage-me/pod-client";
 import { usePod } from "../hooks/usePod";
+import { useState } from "react";
 
-const requestAccess = async (pod: UmaPod) => {
+const requestAccess = async (
+  pod: UmaPod,
+  forWebId: string,
+  forFile: string
+) => {
   console.log("In request access");
-
-  await pod.grantAccess("https://epc.datavillage.me/webid", [
-    "https://storage.sandbox-nl-pod.datanutsbedrijf.be/b6738617-737c-4a03-8178-b21543efe44b/sndk/userProfile.ttl",
-  ]);
+  const fileUrl = new URL(forFile, pod.podUrl).toString();
+  await pod.grantAccess(forWebId, [fileUrl]);
 };
 
 export default function RequestAccess() {
   const pod = usePod() as UmaPod;
+  const [forWebId, setForWebId] = useState<string>();
+  const [forFile, setForFile] = useState<string>();
 
   if (!pod) {
     return <>Not logged in</>;
   }
 
   const startRequest = async () => {
-    await requestAccess(pod);
+    if (forWebId && forFile) {
+      await requestAccess(pod, forWebId, forFile);
+    }
   };
-  console.log("pod in requestaccess", pod);
+
+  const updateForWebId = (value: string) => {
+    setForWebId(value);
+  };
+
+  const updateForFile = (value: string) => {
+    setForFile(value);
+  };
+
   return (
     <>
       <p>Logged in as {pod.userWebId}</p>
       <p>Pod located at {pod.podUrl}</p>
+      For webId
+      <br />
+      <input onChange={(e) => updateForWebId(e.target.value)}></input>
+      <br />
+      For file
+      <br />
+      <input onChange={(e) => updateForFile(e.target.value)} />
+      <br />
       <button onClick={startRequest}>Request access</button>
     </>
   );
